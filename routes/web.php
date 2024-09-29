fb00b33a1b86a2a7e0a5b22f5d4c1d373457954e
Len Woodward <Len@ProjektGopher.com>
2024-09-29 14:31:06 -0700

    add subscribe route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SearchForPhoneNumber;
+use App\Http\Controllers\SubscribeToPhoneNumber;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::post('/search', SearchForPhoneNumber::class);
+Route::post('/subscribe', SubscribeToPhoneNumber::class)->name('subscribe');
