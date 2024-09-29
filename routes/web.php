54ae1b9806f26bd042d86c4a3f3a6ace970111eb
Len Woodward <Len@ProjektGopher.com>
2024-09-29 14:08:11 -0700

    add search route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SearchForPhoneNumber;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/search', SearchForPhoneNumber::class);
