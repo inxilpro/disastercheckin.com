aa1a5583b3e0ddfb280548a15e0e1aac2156ab79
Sheng-Liang Slogar <sheng@diglactic.com>
2024-09-29 18:25:32 -0500

    Refactor normalization to use phone number library

--- app/Http/Controllers/SearchForPhoneNumber.php
+++ app/Http/Controllers/SearchForPhoneNumber.php
@@ -2,28 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Actions\NormalizePhoneNumber;
 use App\Events\UserSearchedForPhoneNumber;
-use Illuminate\Http\Request;
+use App\Http\Requests\SearchForPhoneNumberRequest;
+use Propaganistas\LaravelPhone\PhoneNumber;
 
 class SearchForPhoneNumber extends Controller
 {
-    public function __construct(protected NormalizePhoneNumber $normalizePhoneNumber)
+    public function __invoke(SearchForPhoneNumberRequest $request)
     {
-
-    }
-    public function __invoke(Request $request)
-    {
-        $validated = $request->validate([
-            'phone_number' => 'required|string',
-        ]);
-
-        // TODO: Do we want to use a package or do anything more robust here?
-        // https://stackoverflow.com/questions/4708248/formatting-phone-numbers-in-php
-        $phone = ($this->normalizePhoneNumber)($validated['phone_number']);
+        $phoneNumber = $request->validated('phone_number');
 
         UserSearchedForPhoneNumber::fire(
-            phone_number: $phone,
+            phone_number: (new PhoneNumber($phoneNumber, 'US'))->formatE164(),
         );
     }
 }
