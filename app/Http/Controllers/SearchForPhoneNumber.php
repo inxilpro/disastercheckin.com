7dadc77555a53a2573d246ebde83e6b37537b2cc
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-09-29 23:42:39 +0200

    Add basic validation and normalization to the searched phone number

--- app/Http/Controllers/SearchForPhoneNumber.php
+++ app/Http/Controllers/SearchForPhoneNumber.php
@@ -7,14 +7,29 @@ use Illuminate\Http\Request;
 
 class SearchForPhoneNumber extends Controller
 {
-    public function __invoke(Request $request, string $phone_number)
+    public function __invoke(Request $request)
     {
-        $request->validate([
-            'phone_number' => 'required|phone_number',
+        $validated = $request->validate([
+            'phone_number' => 'required|string',
         ]);
 
+        // TODO: Do we want to use a package or do anything more robust here?
+        // https://stackoverflow.com/questions/4708248/formatting-phone-numbers-in-php
+        $phone = $this->normalizePhoneNumber($validated['phone_number']);
+
+
         UserSearchedForPhoneNumber::fire(
-            phone_number: $phone_number,
+            phone_number: $phone,
         );
+    }
+
+    protected function normalizePhoneNumber($phone) {
+        $phone = preg_replace('/\D/', '', $phone);
+
+        if (strlen($phone) === 10) {
+            $phone = '1' . $phone;
+        }
+
+        return '+' . $phone;
     }
 }
