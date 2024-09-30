1fc9cf60b1963dd7cc7357066f232fae98d06e85
Chris Morrell <inxilpro@users.noreply.github.com>
2024-09-30 12:44:47 -0400

    Bust cache in opt-out

--- app/Events/OptOutRequested.php
+++ app/Events/OptOutRequested.php
@@ -5,6 +5,7 @@ namespace App\Events;
 use App\Data\SmsCommand;
 use App\Models\PhoneNumber;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Thunk\Verbs\Event;
 
 class OptOutRequested extends Event
@@ -29,6 +30,8 @@ class OptOutRequested extends Event
         if ($phone_number = PhoneNumber::findByValue($this->phone_number)) {
             $phone_number->check_ins()->delete();
             $phone_number->update(['is_opted_out' => true]);
+
+            Cache::forget("phone-number-view:{$phone_number->value}");
         }
 
         return true;
