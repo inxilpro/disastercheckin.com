85ea4f100deaa84e8d472606000dcca3d3453b86
Chris Morrell <inxilpro@users.noreply.github.com>
2024-09-30 17:04:46 -0400

    (980) 324-2832

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,14 @@
 
     <div class="border rounded-md border-amber-600 px-4 py-2.5 bg-yellow-50 shadow-sm">
         <h2 class="text-amber-900">
-            Text this number if you are safe <strong class="whitespace-nowrap">(828) 888-0440</strong>
+            Text this number if you are safe <strong class="whitespace-nowrap">(980) 324-2832</strong>
         </h2>
     </div>
 
     <div class="prose-sm max-w-lg mt-6">
         <p>
-            If you are looking for information on a loved one, they may have checked in with us. We have provided a phone
+            If you are looking for information on a loved one, they may have checked in with us. We have provided a
+            phone
             number to local media where people can send SMS updates about their well-being. You can search for your
             loved on
             by your user's phone number below:
