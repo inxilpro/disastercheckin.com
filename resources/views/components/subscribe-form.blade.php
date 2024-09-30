7d3fc5c0476678132ef239e7237ffcba17cdbfb8
hans-lts <hlinkllc@gmail.com>
2024-09-30 14:26:20 -0500

    SVG sizing

--- resources/views/components/subscribe-form.blade.php
+++ resources/views/components/subscribe-form.blade.php
@@ -31,7 +31,7 @@
     @if(session()->get('message.success'))
         <div class="mt-4 flex items-start sm:items-center gap-2 justify-end">
             <span class="text-sm text-green-600 font-medium">{{ session()->get('message.success') }}</span>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 text-green-600">
+            <svg style="height: 1rem; width: 1rem;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 text-green-600">
                 <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm3.844-8.791a.75.75 0 0 0-1.188-.918l-3.7 4.79-1.649-1.833a.75.75 0 1 0-1.114 1.004l2.25 2.5a.75.75 0 0 0 1.15-.043l4.25-5.5Z" clip-rule="evenodd" />
             </svg>
         </div>
