0c61396e8d5bff339aa46596e6a2a148caef4b41
Sheng-Liang Slogar <sheng@diglactic.com>
2024-09-29 19:30:45 -0500

    Fix `phone` length to be 12 characters for E164

---
+++ database/migrations/2024_09_30_002933_update_phone_length_on_phone_numbers_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\PhoneNumber;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('phone_numbers', function (Blueprint $table) {
+            $table->string('phone', 12)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('phone_numbers', function (Blueprint $table) {
+            $table->string('phone', 10)->change();
+        });
+    }
+};
