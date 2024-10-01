98b80842ee00c4ce2748a704b8b098a53ca32e24
Chris Morrell <inxilpro@users.noreply.github.com>
2024-10-01 15:31:07 -0400

    Add cache headers to home view

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SearchController;
 use App\Http\Controllers\AnalyticsController;
-use App\Http\Controllers\SubscribeController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PhoneNumberController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\SubscribeController;
+use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', HomeController::class)->name('home');
 
 Route::post('/search', SearchController::class)->name('search');
 Route::get('/stats', AnalyticsController::class);
@@ -16,4 +17,3 @@ Route::get('/{phone_number}', PhoneNumberController::class)
     ->where('phone_number', '[0-9\+]+');
 
 Route::post('/subscribe', SubscribeController::class)->name('subscribe');
-
