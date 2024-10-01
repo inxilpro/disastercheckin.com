1f7c416d6ba3ec36aed59e7b39689021edcf144a
hans-lts <hlinkllc@gmail.com>
2024-10-01 10:21:50 -0500

    Trimmed for 160 chars

--- lang/en/sms.php
+++ lang/en/sms.php
@@ -13,7 +13,7 @@ return [
 
     'opt-out' => 'Any updates you have sent will be removed from the disaster check-in website shortly.',
     // TODO: Info needs a thorough revamping
-    'info' => 'To share an update on the disaster check-in website, start your message with "UPDATE" (eg. UPDATE I am doing OK!). Your message will be publicly available.',
+    'info' => 'To send updates on DisasterCheckin site to anyone who knows your number, start your msg with UPDATE (UPDATE I am OK) SEARCH to find others (SEARCH 8285550000)',
     'check-in-received' => 'Your update has been saved. Anyone with your phone number can find your message at the disaster check-in website. Send "CANCEL" to remove all your updates.',
     'search' => [
         'invalid' => 'The phone number you searched for appears to be invalid.',
