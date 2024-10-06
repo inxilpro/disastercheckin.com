f6dc64e224d7e6a8d894653ab8aa379d1463f5be
Chris Morrell <inxilpro@users.noreply.github.com>
2024-10-06 09:59:48 -0400

    ugh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,8 @@ namespace App\Providers;
 
 use App\Models\PhoneNumber;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
-
-use function Pest\Laravel\artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,20 +17,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        // $db_file = database_path('disaster.sqlite');
-
-        // if ( !file_exists($db_file) ) {
-        //     file_put_contents($db_file, '');
-        // }
-
-        config([
-            'app.debug' => true,
-            'database.default' => 'sqlite',
-            'database.connections.sqlite.database' => database_path('disaster.sqlite'),
-        ]);
-
-        // Artisan::call('migrate');
-
         Blade::stringable(PhoneNumber::class, fn (PhoneNumber $phone_number) => $phone_number->value->formatNational());
 
         require_once __DIR__.'/../helpers.php';
