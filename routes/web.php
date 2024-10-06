142bbd8cbaa0d960371b9fea8ef959cd15ffc496
Daniel Coulbourne <daniel.coulbourne@gmail.com>
2024-10-06 05:43:26 -0400

    WIP

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,35 @@
 <?php
 
+use App\Services\GoogleDocsService;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\AnalyticsController;
+use App\Http\Controllers\SubscribeController;
+use App\Http\Controllers\GoogleDocsController;
+use App\Http\Controllers\PhoneNumberController;
+use App\Livewire\Water\BarrelsTable;
 
-Route::view('/', 'welcome');
+Route::post('/search', SearchController::class)->name('search');
+Route::get('/stats', AnalyticsController::class);
+Route::get('/docs/{format?}', GoogleDocsController::class)->name('docs');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('water', BarrelsTable::class)->name('water');
+ 
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Route::get('/{phone_number}', PhoneNumberController::class)
+    ->name('phone-number')
+    ->where('phone_number', '[0-9\+]+');
 
-require __DIR__.'/auth.php';
+Route::post('/subscribe', SubscribeController::class)->name('subscribe');
+
+// Route::view('dashboard', 'dashboard')
+// ->middleware(['auth', 'verified'])
+// ->name('dashboard');
+
+// Route::view('profile', 'profile')
+// ->middleware(['auth'])
+// ->name('profile');
+
+// require __DIR__.'/auth.php';
+
+
