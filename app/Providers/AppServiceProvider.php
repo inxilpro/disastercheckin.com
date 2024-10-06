b48151dbefd9a9df3c7bd193a7f140175ec0f686
Daniel Coulbourne <daniel.coulbourne@gmail.com>
2024-10-06 01:01:34 -0400

    WIP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,19 +20,19 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        $db_file = database_path('disaster.sqlite');
+        // $db_file = database_path('disaster.sqlite');
 
-        if ( !file_exists($db_file) ) {
-            file_put_contents($db_file, '');
-        }
+        // if ( !file_exists($db_file) ) {
+        //     file_put_contents($db_file, '');
+        // }
 
-        config([
-            'app.debug' => true,
-            'database.default' => 'sqlite',
-            'database.connections.sqlite.database' => database_path('disaster.sqlite'),
-        ]);
+        // config([
+        //     'app.debug' => true,
+        //     'database.default' => 'sqlite',
+        //     'database.connections.sqlite.database' => database_path('disaster.sqlite'),
+        // ]);
 
-        Artisan::call('migrate');
+        // Artisan::call('migrate');
 
         Blade::stringable(PhoneNumber::class, fn (PhoneNumber $phone_number) => $phone_number->value->formatNational());
 
